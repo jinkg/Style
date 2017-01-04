5b394ccfc5452f7a1aa526577230e97f128d5730
jinyalin <jinyalin@kanzhun.com>
2017-01-04 19:42:04 +0800

    update main activity

---
+++ app/src/main/java/com/yalin/style/ui/AnimatedStyleLogoFragment.java
@@ -0,0 +1,74 @@
+package com.yalin.style.ui;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Interpolator;
+import android.view.animation.OvershootInterpolator;
+import android.widget.ImageView;
+import com.yalin.style.R;
+
+/**
+ * YaLin 2017/1/4.
+ */
+
+public class AnimatedStyleLogoFragment extends Fragment {
+
+  private Runnable mOnFillStartedCallback;
+  private View mSubtitleView;
+  private ImageView mLogoView;
+  private float mInitialLogoOffset;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    mInitialLogoOffset = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16,
+        getResources().getDisplayMetrics());
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    View rootView = inflater.inflate(R.layout.animated_logo_fragment, container, false);
+    mSubtitleView = rootView.findViewById(R.id.logo_subtitle);
+
+    mLogoView = (ImageView) rootView.findViewById(R.id.logo_view);
+    reset();
+    return rootView;
+  }
+
+  public void start() {
+    mLogoView.animate().translationY(0).setListener(new AnimatorListenerAdapter() {
+      @Override
+      public void onAnimationEnd(Animator animation) {
+        mSubtitleView.setVisibility(View.VISIBLE);
+        mSubtitleView.setTranslationY(-mSubtitleView.getHeight());
+        Interpolator interpolator = new OvershootInterpolator();
+        mSubtitleView.animate().translationY(0).setInterpolator(interpolator).setDuration(500)
+            .start();
+
+        if (mOnFillStartedCallback != null) {
+          mOnFillStartedCallback.run();
+        }
+
+      }
+    }).setDuration(500).start();
+
+  }
+
+  public void setOnFillStartedCallback(Runnable fillStartedCallback) {
+    mOnFillStartedCallback = fillStartedCallback;
+  }
+
+  public void reset() {
+    mLogoView.setTranslationY(mInitialLogoOffset);
+    mSubtitleView.setVisibility(View.INVISIBLE);
+  }
+}
---
+++ app/src/main/java/com/yalin/style/ui/StyleActivity.java
@@ -0,0 +1,105 @@
+package com.yalin.style.ui;
+
+import android.app.WallpaperManager;
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.yalin.style.R;
+import com.yalin.style.StyleWallpaperService;
+import com.yalin.style.service.BootstrapService;
+
+public class StyleActivity extends AppCompatActivity implements OnClickListener {
+
+  private RelativeLayout mMainContainer;
+  private View mActiveContainer;
+  private Button mActiveButton;
+
+  private Handler mHandler = new Handler();
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity_main);
+
+    mMainContainer = (RelativeLayout) findViewById(R.id.main_container);
+    int flags = 0;
+    flags |= View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+    mMainContainer.setSystemUiVisibility(flags);
+
+    setUpActiveView();
+
+    updateUi();
+  }
+
+  private void setUpActiveView() {
+    mActiveContainer = findViewById(R.id.active_container);
+    mActiveButton = (Button) findViewById(R.id.activate_style_button);
+    mActiveButton.setOnClickListener(this);
+  }
+
+  private void updateUi() {
+    mActiveContainer.animate()
+        .alpha(1)
+        .setDuration(1000)
+        .start();
+
+    final AnimatedStyleLogoFragment logoFragment = (AnimatedStyleLogoFragment) getFragmentManager()
+        .findFragmentById(R.id.animated_logo_fragment);
+    logoFragment.reset();
+    logoFragment.setOnFillStartedCallback(new Runnable() {
+      @Override
+      public void run() {
+        mActiveButton.animate().alpha(1f).setDuration(500);
+      }
+    });
+    mHandler.postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        logoFragment.start();
+      }
+    }, 1000);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    BootstrapService.startBootstrapIfNecessary(this);
+  }
+
+  private void setWallpaper() {
+    try {
+      startActivity(new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER)
+          .putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+              new ComponentName(StyleActivity.this, StyleWallpaperService.class))
+          .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    } catch (ActivityNotFoundException e) {
+      try {
+        startActivity(new Intent(WallpaperManager.ACTION_LIVE_WALLPAPER_CHOOSER)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+      } catch (ActivityNotFoundException e2) {
+        Toast.makeText(StyleActivity.this, "xxx", Toast.LENGTH_LONG).show();
+      }
+    }
+  }
+
+  @Override
+  public void onClick(View v) {
+    switch (v.getId()) {
+      case R.id.activate_style_button:
+        setWallpaper();
+        break;
+    }
+  }
+}
