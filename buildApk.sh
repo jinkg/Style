c7c28949f5f4eed964c1e28ef1f15397ffbdaed1
jinyalin <jinyalin@baidu.com>
2017-05-19 18:20:00 +0800

    update app name

--- buildApk.sh
+++ buildApk.sh
@@ -3,11 +3,11 @@ echo "Start Build Apk"
 
 ./gradlew clean
 ./gradlew assembleProductionRelease -Dchannel=yingyongbao
-./gradlew assembleProductionRelease -Dchannel=360
+./gradlew assembleProductionRelease -Dchannel=360 -Dapp_name=Style艺术壁纸
 ./gradlew assembleProductionRelease -Dchannel=vivo,pc6
 ./gradlew assembleProductionRelease -Dchannel=flyme
 ./gradlew assembleProductionRelease -Dchannel=wandoujia
-./gradlew assembleProductionRelease -Dchannel=baidu
+./gradlew assembleProductionRelease -Dchannel=baidu -Dapp_name=Style艺术壁纸
 ./gradlew assembleProductionRelease -Dchannel=kinglloy
 ./gradlew assembleProductionRelease -Dchannel=google
 
