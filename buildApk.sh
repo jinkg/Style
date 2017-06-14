e3481d4f24c3b98b8bc84444f1ec59b47e876573
jinyalin <jinyalin@baidu.com>
2017-06-14 16:37:20 +0800

    add huawei channel

--- buildApk.sh
+++ buildApk.sh
@@ -4,7 +4,7 @@ echo "Start Build Apk"
 ./gradlew clean
 ./gradlew assembleProductionRelease -Dchannel=yingyongbao
 ./gradlew assembleProductionRelease -Dchannel=360 -Dapp_name=Style艺术壁纸
-./gradlew assembleProductionRelease -Dchannel=vivo,pc6
+./gradlew assembleProductionRelease -Dchannel=vivo
 ./gradlew assembleProductionRelease -Dchannel=flyme
 ./gradlew assembleProductionRelease -Dchannel=wandoujia
 ./gradlew assembleProductionRelease -Dchannel=baidu -Dapp_name=Style艺术壁纸
