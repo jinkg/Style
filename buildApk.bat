fec15e07db3d67d8f125260f4b995c133874b964
jinyalin <jinyalin@baidu.com>
2017-08-06 13:49:55 +0800

    update buildApk script

--- buildApk.bat
+++ buildApk.bat
@@ -2,11 +2,14 @@
 echo Start Build Apk
 
 call gradlew clean
+call gradlew assembleProductionRelease -Dchannel=kinglloy
 call gradlew assembleProductionRelease -Dchannel=yingyongbao
-call gradlew assembleProductionRelease -Dchannel=360
-call gradlew assembleProductionRelease -Dchannel=vivo,pc6
+call gradlew assembleProductionRelease -Dchannel=360 -Dapp_name=Style艺术壁纸
+call gradlew assembleProductionRelease -Dchannel=vivo
 call gradlew assembleProductionRelease -Dchannel=flyme
 call gradlew assembleProductionRelease -Dchannel=wandoujia
-call gradlew assembleProductionRelease -Dchannel=baidu
+call gradlew assembleProductionRelease -Dchannel=baidu -Dapp_name=Style艺术壁纸
+call gradlew assembleGoogleprodRelease -Dchannel=google
+call gradlew assembleProductionRelease -Dchannel=huawei -Dapp_name=Style艺术壁纸
 
 echo Build Apk Complete
